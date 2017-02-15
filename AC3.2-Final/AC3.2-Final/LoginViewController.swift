6b68c6ef159fb5bd633e587017bab4293062298b
Madushani Lekam Wasam Liyanage <Shashi@Shashis-MacBook.local>
2017-02-15 15:23:16 -0500

    Mostly Done

---
+++ AC3.2-Final/AC3.2-Final/LoginViewController.swift
@@ -0,0 +1,84 @@
+//
+//  LoginViewController.swift
+//  AC3.2-Final
+//
+//  Created by Madushani Lekam Wasam Liyanage on 2/15/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class LoginViewController: UIViewController, UIAlertViewDelegate {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if let error = error {
+                    print("User Login Error \(error.localizedDescription)")
+                    let alertController = showAlert(title: "Login Failed", message: "Failed to Login. Please Check Your Email and Password!")
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                    self.clearTextFields()
+                }
+                else {
+                    
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let svc = storyboard.instantiateViewController(withIdentifier: "TabBarVC")
+                    let alertController = UIAlertController(title: "Login Successful!", message: nil, preferredStyle: .alert)
+                    self.present(alertController, animated: true, completion: nil)
+                    self.clearTextFields()
+                    
+                    alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+                        self.present(svc, animated: true, completion: nil)
+                    }))
+                    
+                }
+            })
+        }
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                if let error = error {
+                    print("User Creating Error \(error.localizedDescription)")
+                    let alertController = showAlert(title: "Signup Failed", message: "Failed to Register. Please Try Again!")
+                    self.present(alertController, animated: true, completion: nil)
+                    self.clearTextFields()
+                }
+                else {
+                    let alertController = showAlert(title: "Signup Success", message: "Successfully Registered. Please Login to Use Our App!")
+                    self.present(alertController, animated: true, completion: nil)
+                    self.clearTextFields()
+                }
+            })
+        }
+    }
+    
+    func clearTextFields() {
+        emailTextField.text = nil
+        passwordTextField.text = nil
+    }
+    
+}
+
+public func showAlert(title: String, message: String?) -> UIAlertController {
+    
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+    alertController.addAction(defaultAction)
+    
+    return alertController
+}
---
+++ AC3.2-Final/AC3.2-Final/UploadViewController.swift
@@ -0,0 +1,114 @@
+//
+//  UploadViewController.swift
+//  AC3.2-Final
+//
+//  Created by Madushani Lekam Wasam Liyanage on 2/15/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+import MobileCoreServices
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
+
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    @IBOutlet weak var selectedImageView: UIImageView!
+    @IBOutlet weak var commentTextField: UITextField!
+    var imagePickerController: UIImagePickerController!
+    
+    var capturedImage: UIImage!
+    var user: FIRUser!
+    var databaseRef: FIRDatabaseReference!
+    var storageRef: FIRStorageReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Unit6Final-staGram"
+        self.user = FIRAuth.auth()?.currentUser
+        self.databaseRef = FIRDatabase.database().reference().child("posts")
+        
+    }
+    
+    @IBAction func pickImageTapped(_ sender: UIButton) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.modalPresentationStyle = .currentContext
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        imagePickerController.mediaTypes = [String(kUTTypeImage)]
+        
+        self.imagePickerController = imagePickerController
+        self.present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
+        shareToFirebase()
+    }
+    
+    func shareToFirebase() {
+        
+        let postRef = self.databaseRef.childByAutoId()
+        let currentUserId = FIRAuth.auth()?.currentUser?.uid
+        let storage = FIRStorage.storage()
+        let storageRef = storage.reference(forURL: "gs://ac-32-final.appspot.com")
+        let imageNameRef = storageRef.child("images/\(postRef.key)")
+        
+        let metadata = FIRStorageMetadata()
+        metadata.cacheControl = "public,max-age=300"
+        metadata.contentType = "image/jpeg"
+        let jpeg = UIImageJPEGRepresentation(capturedImage, 0.5)
+        
+        let _ = imageNameRef.put(jpeg!, metadata: metadata, completion: { (metadata, error) in
+            guard metadata != nil else {
+                print("put error: \(error?.localizedDescription)")
+                return
+            }
+        })
+
+        let post = Post(key: postRef.key, comment: self.commentTextField.text!, userId: currentUserId!)
+        let dict = post.addDictionary
+        
+        postRef.setValue(dict) { (error, ref) in
+            if let error = error {
+                print("Set value error: \(error.localizedDescription)")
+                let alertController = showAlert(title: "Upload Failed", message: "Uploading Failed! Please Try Again!")
+                self.present(alertController, animated: true, completion: nil)
+            }
+            else {
+                let alertController = showAlert(title: "Photo Uploaded!", message: nil)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        switch info[UIImagePickerControllerMediaType] as! String {
+        case String(kUTTypeImage):
+            if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                self.capturedImage = image
+                selectedImageView.image = nil
+                selectedImageView.backgroundColor = .clear
+                selectedImageView.contentMode = .scaleAspectFit
+                selectedImageView.image = capturedImage
+                
+            }
+            
+        default:
+            print("Bad Media Type")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
